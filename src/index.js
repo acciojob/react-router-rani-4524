873410d09693745ca143cabe2084f743f92a49e5
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2026-02-12 21:28:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+
+import { StrictMode } from 'react'
+
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { BrowserRouter } from 'react-router-dom'
