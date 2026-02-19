d8ab27586049c1fbaee962c5104f9358c6355a47
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2026-02-19 15:49:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,19 @@ import React from "react";
 import './../styles/App.css';
 import Home from './Home'
 import About from './About'
-import { Route, Switch ,Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navigation from './Navigation'
 
 const App = () => {
   return (
-    <div>
-    
-     
-      <ul>
-              <li><Link to ='/'>Home</Link></li>
-              <li><Link to ='/about'>About</Link></li>
-      </ul>
-
-        <Switch>
-          <Route exact path="/" component={<Home/>} />
-    <Route path='/about' element={<About/>} />
-        </Switch>
+    <BrowserRouter>
+    <Navigation/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
+      </BrowserRouter>
    
-  
-    </div>
   )
 }
 
