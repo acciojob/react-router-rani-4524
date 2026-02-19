1d0a2f6bfa3404e971a6299088cc9fc2bcf4f27c
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2026-02-19 15:45:17 +0530

    Create Navigation.jsx

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>
+          <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/about'>About</Link></li>
+          </ul>
+    </div>
+  )
+}
+
+export default Navigation
